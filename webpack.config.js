5914af811dc7aa81345c293179707ae2a2e11060
Seth Shoemaker <Shoemakerseth2003@gmail.com>
2022-12-15 23:25:47 -0500

    Added images loading to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,35 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const glob = require("glob");
 
 module.exports = {
     mode: process.env.NODE_ENV,
-    entry: [
-        path.resolve(__dirname, './src/js/app.js'),
-        path.resolve(__dirname, './src/sass/main.scss')
-    ],
+    entry: [].concat(
+        [
+            path.resolve(__dirname, './src/js/app.js'),
+            path.resolve(__dirname, './src/sass/main.scss'),
+        ],
+        glob.sync("./src/images/**/*.*")
+    ),
     module: {
         rules: [
             {
                 test: /\.scss$/,
                 exclude: '/node_modules',
                 use: [ MiniCssExtractPlugin.loader, "css-loader", "sass-loader" ]
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [ 
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "images/[folder]/[name].[ext]",
+                        }
+                    },
+                    "image-webpack-loader"
+                ],
             }
         ]
     },
@@ -24,5 +40,9 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: "./src/index.html"
         })
-    ]
+    ],
+
+    devServer : {
+        liveReload: true
+    }
 }
