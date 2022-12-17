554cad994ade6c2068518b6e946062d9f887ade7
Seth Shoemaker <Shoemakerseth2003@gmail.com>
2022-12-17 18:03:39 -0500

    remove webpack from technologies

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,7 @@
 export const projects = [
     {
         "name": "Shifterr",
-        "technologies": [ "C#", "ASP.NET", "Docker", "MySQL", "AWS", "Webpack", "TypeScript", "Angular", "SASS" ],
+        "technologies": [ "C#", "ASP.NET", "Docker", "MySQL", "AWS", "Angular", "SASS" ],
         "description": "Shifterr is a work scheduling application. This project showcases multi-tenancy, authentication, and use of a database.",
         "live" : "https://app.shifterr.net",
         "repo" : "https://github.com/SethShoemaker/Shifterr",
@@ -9,7 +9,7 @@ export const projects = [
     },
     {
         "name": "Blunderr", 
-        "technologies": [ "PHP", "Laravel", "MySQL", "AWS", "Webpack", "jQuery", "SASS" ],
+        "technologies": [ "PHP", "Laravel", "MySQL", "AWS", "jQuery", "SASS" ],
         "description" : "Blunderr is a bug tracking application. This project showcases multi-tenancy, authentication, and use of a database.",
         "live": "https://blunderr.net",
         "repo" : "https://github.com/SethShoemaker/Blunderr",
@@ -17,7 +17,7 @@ export const projects = [
     },
     {
         "name" : "Zesty",
-        "technologies": [ "PHP", "Laravel", "MySQL", "AWS", "Webpack", "jQuery", "SASS" ],
+        "technologies": [ "PHP", "Laravel", "MySQL", "AWS", "jQuery", "SASS" ],
         "description": "Zesty is a recipe sharing app. This project showcases authentication and use of a database.",
         "live" : "https://zesty.cooking",
         "repo" : "https://github.com/SethShoemaker/Zesty",
